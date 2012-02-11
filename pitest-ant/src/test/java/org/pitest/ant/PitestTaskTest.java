e34278c0097230790e4d6ee64dec9168f9017222
henry <devnull@localhost>
2012-02-11 20:15:23 +0000

    added licence notice and fix for environment dependent test

--- pitest-ant/src/test/java/org/pitest/ant/PitestTaskTest.java
+++ pitest-ant/src/test/java/org/pitest/ant/PitestTaskTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Nicolas Rusconi
+ * 
+ * Licensed under the Apache License, Version 2.0 ("the "License""); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
+ */
+
 package org.pitest.ant;
 
 import static org.mockito.Matchers.argThat;
@@ -13,7 +28,6 @@ import org.apache.tools.ant.taskdefs.Java;
 import org.apache.tools.ant.types.Commandline.Argument;
 import org.apache.tools.ant.types.Path;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -183,9 +197,8 @@ public class PitestTaskTest {
   }
 
   @Test
-  @Ignore("broken!")
   public void shouldSetClasspathOnJavaTask() throws Exception {
-    final String classpath = "bin/;lib/util.jar";
+    final String classpath = "bin/" + File.pathSeparator + "lib/util.jar";
     this.pitestTask.setClasspath(classpath);
     this.pitestTask.execute(this.java);
 
@@ -207,17 +220,16 @@ public class PitestTaskTest {
 
   private static class PathMatcher extends ArgumentMatcher<Path> {
 
-    private static final String PATH_SEPARATOR = ";";
     private final String[]      expectedPaths;
 
     public PathMatcher(final String path) {
-      this.expectedPaths = path.split(PATH_SEPARATOR);
+      this.expectedPaths = path.split(File.pathSeparator);
     }
 
     @Override
     public boolean matches(final Object argument) {
       final Path argPath = (Path) argument;
-      final String[] paths = argPath.toString().split(PATH_SEPARATOR);
+      final String[] paths = argPath.toString().split(File.pathSeparator);
       final boolean matches = paths.length == this.expectedPaths.length;
       if (matches) {
         for (final String expectedPathElement : this.expectedPaths) {
