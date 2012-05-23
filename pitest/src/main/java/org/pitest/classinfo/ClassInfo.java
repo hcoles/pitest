f5794d8a4a598cd00bb9a99ee7ff61d90286e3d1
henry.coles <devnull@localhost>
2012-05-23 22:31:06 +0100

    restored equals and hashcode

--- pitest/src/main/java/org/pitest/classinfo/ClassInfo.java
+++ pitest/src/main/java/org/pitest/classinfo/ClassInfo.java
@@ -148,4 +148,31 @@ public class ClassInfo {
     };
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ClassInfo other = (ClassInfo) obj;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    return true;
+  }
+
+  
+  
 }
