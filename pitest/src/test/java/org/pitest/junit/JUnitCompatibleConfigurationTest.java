ecdedb22f1bf6ce25e13dc6fe858fe30baa2142c
henry <devnull@localhost>
2012-09-09 21:38:00 +0100

    added tests missed from commit

---
+++ pitest/src/test/java/org/pitest/junit/JUnitCompatibleConfigurationTest.java
@@ -0,0 +1,40 @@
+package org.pitest.junit;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JUnitCompatibleConfigurationTest {
+  JUnitCompatibleConfiguration testee;
+
+  @Before
+  public void setUp() throws Exception {
+
+    this.testee = new JUnitCompatibleConfiguration();
+  }
+
+  @Test
+  public void considersPre46Invalid() throws Exception {
+    assertThat(this.testee.isInvalidVersion("4.5"), is(true));
+    assertThat(this.testee.isInvalidVersion("4.5-SNAPSHOT"), is(true));
+  }
+
+  @Test
+  public void canParseReleaseVersion() throws Exception {
+    assertThat(this.testee.isInvalidVersion("4.6"), is(false));
+    assertThat(this.testee.isInvalidVersion("4.10"), is(false));
+  }
+
+  @Test
+  public void canParseSnapshotVersion() throws Exception {
+    assertThat(this.testee.isInvalidVersion("4.10-SNAPSHOT"), is(false));
+    assertThat(this.testee.isInvalidVersion("4.5-SNAPSHOT"), is(true));
+  }
+
+  @Test
+  public void canParseReleaseCandidateVersion() throws Exception {
+    assertThat(this.testee.isInvalidVersion("4.10.rc1"), is(false));
+  }
+}
