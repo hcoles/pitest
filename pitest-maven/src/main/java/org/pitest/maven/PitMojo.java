c63c43ee153c6458b194b8c1bdb6c07393ae2523
henry <devnull@localhost>
2013-02-18 22:21:31 +0000

    remove accidentally commited code

--- pitest-maven/src/main/java/org/pitest/maven/PitMojo.java
+++ pitest-maven/src/main/java/org/pitest/maven/PitMojo.java
@@ -254,9 +254,6 @@ public class PitMojo extends AbstractMojo {
   public void execute() throws MojoExecutionException {
     final ReportOptions data = new MojoToReportOptionsConverter(this).convert();
     this.goalStrategy.execute( detectBaseDir() ,data);
-    for ( Object each : this.project.getCollectedProjects() ) {
-      System.out.println("Project " + each);
-    }
   }
 
   protected File detectBaseDir() {
