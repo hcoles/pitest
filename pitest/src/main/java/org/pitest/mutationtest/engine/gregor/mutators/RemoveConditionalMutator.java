62c86984fb21c3087b61b152ee705f4455532193
Rahul <gopinath@eecs.oregonstate.edu>
2014-04-01 18:10:27 -0700

    Extend Remove Conditional
    Extend Remove Conditional so that it is able to do parital (==,!=) and full (<,>,<=,>=)
    mutations along with the option of replacing the conditional with either true or false.

--- pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/RemoveConditionalMutator.java
+++ pitest/src/main/java/org/pitest/mutationtest/engine/gregor/mutators/RemoveConditionalMutator.java
@@ -1,5 +1,7 @@
 package org.pitest.mutationtest.engine.gregor.mutators;
 
+import java.util.List;
+import java.util.ArrayList;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
@@ -8,9 +10,33 @@ import org.pitest.mutationtest.engine.gregor.Context;
 import org.pitest.mutationtest.engine.gregor.MethodInfo;
 import org.pitest.mutationtest.engine.gregor.MethodMutatorFactory;
 
-public enum RemoveConditionalMutator implements MethodMutatorFactory {
+public class RemoveConditionalMutator implements MethodMutatorFactory {
 
-  REMOVE_CONDITIONALS_MUTATOR;
+  //REMOVE_CONDITIONALS_MUTATOR;
+
+  // EQUAL : Mutate only equality operators
+  // ORDER : Mutate only Ordering operators
+  public enum Choice {EQUAL, ORDER}
+
+  final Choice kind;
+  final boolean replaceWith; // Default is true
+
+  public RemoveConditionalMutator(final Choice c, final boolean rc) {
+    kind = c;
+    replaceWith = rc;
+  }
+
+  public static Iterable<MethodMutatorFactory> makeMutators() {
+    List<MethodMutatorFactory> variations = new ArrayList<MethodMutatorFactory>();
+    Choice [] allChoices = {Choice.EQUAL, Choice.ORDER};
+    boolean [] arrWith = {true, false};
+    for (Choice c : allChoices) {
+      for (boolean b : arrWith) {
+        variations.add(new RemoveConditionalMutator(c, b));
+      }
+    }
+    return variations;
+  }
 
   public MethodVisitor create(final Context context,
       final MethodInfo methodInfo, final MethodVisitor methodVisitor) {
@@ -18,74 +44,91 @@ public enum RemoveConditionalMutator implements MethodMutatorFactory {
   }
 
   public String getGloballyUniqueId() {
-    return this.getClass().getName();
+    return this.getClass().getName() + "_" + kind + "_" + (replaceWith ? "IF" : "ELSE");
   }
 
   public String getName() {
-    return name();
+    return "REMOVE_CONDITIONALS_" + kind + "_" + (replaceWith ? "IF" : "ELSE") + "_MUTATOR";
   }
 
-}
+  private final class RemoveConditionalMethodVisitor extends MethodVisitor {
 
-class RemoveConditionalMethodVisitor extends MethodVisitor {
+    private static final String        DESCRIPTION = "removed conditional";
+    private final Context              context;
+    private final MethodMutatorFactory factory;
 
-  private static final String        DESCRIPTION = "removed conditional";
-  private final Context              context;
-  private final MethodMutatorFactory factory;
+    public RemoveConditionalMethodVisitor(final MethodMutatorFactory factory,
+        final Context context, final MethodVisitor delegateMethodVisitor) {
+      super(Opcodes.ASM4, delegateMethodVisitor);
+      this.context = context;
+      this.factory = factory;
+    }
 
-  public RemoveConditionalMethodVisitor(final MethodMutatorFactory factory,
-      final Context context, final MethodVisitor delegateMethodVisitor) {
-    super(Opcodes.ASM4, delegateMethodVisitor);
-    this.context = context;
-    this.factory = factory;
-  }
+    @Override
+    public void visitJumpInsn(final int opcode, final Label label) {
 
-  @Override
-  public void visitJumpInsn(final int opcode, final Label label) {
+      if (canMutate(opcode)) {
+        final MutationIdentifier newId = this.context.registerMutation(
+            this.factory, DESCRIPTION);
 
-    if (canMutate(opcode)) {
-      final MutationIdentifier newId = this.context.registerMutation(
-          this.factory, DESCRIPTION);
-
-      if (this.context.shouldMutate(newId)) {
-        emptyStack(opcode);
+        if (this.context.shouldMutate(newId)) {
+          emptyStack(opcode);
+          if (!replaceWith) {
+            super.visitJumpInsn(Opcodes.GOTO, label);
+          }
+        } else {
+          this.mv.visitJumpInsn(opcode, label);
+        }
       } else {
         this.mv.visitJumpInsn(opcode, label);
       }
-    } else {
-      this.mv.visitJumpInsn(opcode, label);
+
+    }
+
+    private void emptyStack(final int opcode) {
+      switch (opcode) {
+        // EQUAL
+        case Opcodes.IF_ICMPNE:
+        case Opcodes.IF_ICMPEQ:
+        case Opcodes.IF_ACMPEQ:
+        case Opcodes.IF_ACMPNE:
+        // ORDER
+        case Opcodes.IF_ICMPGE:
+        case Opcodes.IF_ICMPGT:
+        case Opcodes.IF_ICMPLE:
+        case Opcodes.IF_ICMPLT:
+          super.visitInsn(Opcodes.POP2);
+          break;
+        default:
+          super.visitInsn(Opcodes.POP);
+      }
+
+    }
+
+    private boolean canMutate(final int opcode) {
+      switch (opcode) {
+        case Opcodes.IFLE:
+        case Opcodes.IFGE:
+        case Opcodes.IFGT:
+        case Opcodes.IFLT:
+        case Opcodes.IF_ICMPGE:
+        case Opcodes.IF_ICMPGT:
+        case Opcodes.IF_ICMPLE:
+        case Opcodes.IF_ICMPLT:
+          return (kind == Choice.ORDER);
+        case Opcodes.IFEQ:
+        case Opcodes.IFNE:
+        case Opcodes.IFNONNULL:
+        case Opcodes.IFNULL:
+        case Opcodes.IF_ICMPNE:
+        case Opcodes.IF_ICMPEQ:
+        case Opcodes.IF_ACMPEQ:
+        case Opcodes.IF_ACMPNE:
+          return (kind == Choice.EQUAL);
+        default:
+          return false;
+      }
     }
 
   }
-
-  private void emptyStack(final int opcode) {
-    switch (opcode) {
-    case Opcodes.IF_ICMPNE:
-    case Opcodes.IF_ICMPEQ:
-    case Opcodes.IF_ACMPEQ:
-    case Opcodes.IF_ACMPNE:
-      super.visitInsn(Opcodes.POP2);
-      break;
-    default:
-      super.visitInsn(Opcodes.POP);
-    }
-
-  }
-
-  private boolean canMutate(final int opcode) {
-    switch (opcode) {
-    case Opcodes.IFEQ:
-    case Opcodes.IFNE:
-    case Opcodes.IFNONNULL:
-    case Opcodes.IFNULL:
-    case Opcodes.IF_ICMPNE:
-    case Opcodes.IF_ICMPEQ:
-    case Opcodes.IF_ACMPEQ:
-    case Opcodes.IF_ACMPNE:
-      return true;
-    default:
-      return false;
-    }
-  }
-
 }
