d458121e5b44b8049f291be40e0a081b0480ce81
Jason Fehr <jasonmfehr@server.fake>
2015-03-10 22:50:43 -0500

    added license info for #10

--- pitest-maven/src/main/java/org/pitest/maven/report/generator/ReportGenerationResultEnum.java
+++ pitest-maven/src/main/java/org/pitest/maven/report/generator/ReportGenerationResultEnum.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2015 Jason Fehr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 package org.pitest.maven.report.generator;
 
 public enum ReportGenerationResultEnum {
@@ -5,4 +19,5 @@ public enum ReportGenerationResultEnum {
 	SUCCESS, 
 	FAILURE, 
 	NOT_EXECUTED;
+	
 }
--- pitest-maven/src/test/java/org/pitest/maven/report/generator/HTMLReportGeneratorTest.java
+++ pitest-maven/src/test/java/org/pitest/maven/report/generator/HTMLReportGeneratorTest.java
@@ -1,6 +1,19 @@
+/*
+ * Copyright 2015 Jason Fehr
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
 package org.pitest.maven.report.generator;
 
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
@@ -30,7 +43,7 @@ public class HTMLReportGeneratorTest {
 	
 	@Test
 	public void testName() {
-		assertThat(fixture.getGeneratorName(), equalTo("HTMLReportGenerator"));
+		assertThat(fixture.getGeneratorName(), is("HTMLReportGenerator"));
 	}
 	
 	@Test
