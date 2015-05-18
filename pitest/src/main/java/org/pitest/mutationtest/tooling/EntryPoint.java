8800585e7590fda63b182a55abed9ce100927611
Lars K.W. Gohlke <lkwg82@gmx.de>
2015-05-18 23:02:29 +0200

    enhanced readability

--- pitest/src/main/java/org/pitest/mutationtest/tooling/EntryPoint.java
+++ pitest/src/main/java/org/pitest/mutationtest/tooling/EntryPoint.java
@@ -7,13 +7,11 @@ import org.pitest.classpath.ProjectClassPaths;
 import org.pitest.coverage.CoverageGenerator;
 import org.pitest.coverage.execute.CoverageOptions;
 import org.pitest.coverage.execute.DefaultCoverageGenerator;
-import org.pitest.functional.Option;
 import org.pitest.mutationtest.HistoryStore;
 import org.pitest.mutationtest.MutationResultListenerFactory;
 import org.pitest.mutationtest.config.PluginServices;
 import org.pitest.mutationtest.config.ReportOptions;
 import org.pitest.mutationtest.config.SettingsFactory;
-import org.pitest.mutationtest.incremental.WriterFactory;
 import org.pitest.mutationtest.incremental.XStreamHistoryStore;
 import org.pitest.process.JavaAgent;
 import org.pitest.process.LaunchOptions;
@@ -22,7 +20,6 @@ import org.pitest.util.Timings;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.Map;
 
 public class EntryPoint {
@@ -62,55 +59,53 @@ public class EntryPoint {
                                 SettingsFactory settings,
                                 Map<String, String> environmentVariables) {
 
-    final ClassPath cp = data.getClassPath();
+    ClassPath classPath = data.getClassPath();
 
-    final Option<Reader> reader = data.createHistoryReader();
-    final WriterFactory historyWriter = data.createHistoryWriter();
+    ProjectClassPaths mutationClassPaths = data.getMutationClassPaths();
+    CoverageOptions coverageOptions = settings.createCoverageOptions();
+    CodeSource code = new CodeSource(mutationClassPaths, coverageOptions.getPitConfig().testClassIdentifier());
 
     // workaround for apparent java 1.5 JVM bug . . . might not play nicely
     // with distributed testing
-    final JavaAgent jac = new JarCreatingJarFinder(
-        new ClassPathByteArraySource(cp));
+    JavaAgent javaAgent = new JarCreatingJarFinder(new ClassPathByteArraySource(classPath));
+    KnownLocationJavaAgentFinder javaAgentFinder = new KnownLocationJavaAgentFinder(javaAgent.getJarLocation().value());
+    LaunchOptions launchOptions = new LaunchOptions( javaAgentFinder,settings.getJavaExecutable(),
+                                                     data.getJvmArgs(),environmentVariables);
+    Timings timings = new Timings();
+    CoverageGenerator coverageDatabase = new DefaultCoverageGenerator(baseDir,
+                                                                      coverageOptions,
+                                                                      launchOptions,
+                                                                      code,
+                                                                      settings.createCoverageExporter(),
+                                                                      timings,
+                                                                      !data.isVerbose());
 
-    final KnownLocationJavaAgentFinder ja = new KnownLocationJavaAgentFinder(
-        jac.getJarLocation().value());
+    HistoryStore history = new XStreamHistoryStore(data.createHistoryWriter(),
+                                                   data.createHistoryReader());
 
-    final ResultOutputStrategy reportOutput = settings.getOutputStrategy();
+    MutationResultListenerFactory reportFactory = settings.createListener();
+    ResultOutputStrategy reportOutput = settings.getOutputStrategy();
+    MutationStrategies strategies = new MutationStrategies( settings.createEngine(),
+                                                            history,
+                                                            coverageDatabase,
+                                                            reportFactory,
+                                                            reportOutput);
 
-    final MutationResultListenerFactory reportFactory = settings
-        .createListener();
-
-    final CoverageOptions coverageOptions = settings.createCoverageOptions();
-    final LaunchOptions launchOptions = new LaunchOptions(ja,settings.getJavaExecutable(), data.getJvmArgs(),environmentVariables);
-    final ProjectClassPaths cps = data.getMutationClassPaths();
-
-    final CodeSource code = new CodeSource(cps, coverageOptions.getPitConfig()
-        .testClassIdentifier());
-
-    final Timings timings = new Timings();
-    final CoverageGenerator coverageDatabase = new DefaultCoverageGenerator(
-        baseDir, coverageOptions, launchOptions, code,
-        settings.createCoverageExporter(), timings, !data.isVerbose());
-
-    final HistoryStore history = new XStreamHistoryStore(historyWriter, reader);
-
-    final MutationStrategies strategies = new MutationStrategies(
-        settings.createEngine(), history, coverageDatabase, reportFactory,
-        reportOutput);
-
-    final MutationCoverage report = new MutationCoverage(strategies, baseDir,
-        code, data, settings, timings);
+    MutationCoverage report = new MutationCoverage(strategies,
+                                                   baseDir,
+                                                   code,
+                                                   data,
+                                                   settings,
+                                                   timings);
 
     try {
       return AnalysisResult.success(report.runReport());
-    } catch (final IOException e) {
+    } catch (IOException e) {
       return AnalysisResult.fail(e);
     } finally {
-      jac.close();
-      ja.close();
-      historyWriter.close();
+      javaAgent.close();
+      javaAgentFinder.close();
+      data.createHistoryWriter().close();
     }
-
   }
-
 }
