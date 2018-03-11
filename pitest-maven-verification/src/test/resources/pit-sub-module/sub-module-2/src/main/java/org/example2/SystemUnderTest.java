503a86cd08e8ac5f567895b0fb172516c23ee75b
sbuisson <sbuisson@xebia.fr>
2018-03-11 21:00:42 +0100

    Adding an additional report-aggregate-module mojo which aggregate using module instead of dependency (add integration tests)

---
+++ pitest-maven-verification/src/test/resources/pit-sub-module/sub-module-2/src/main/java/org/example2/SystemUnderTest.java
@@ -0,0 +1,24 @@
+package org.example2;
+
+public class SystemUnderTest {
+  
+  private int aNumber = 0;
+  
+  public SystemUnderTest() {
+    super();
+    
+    int a = 25;
+    int b = 10;
+    if (a < b) {
+      aNumber = 10;
+    } else {
+      aNumber = -25;
+    }
+  }
+  
+  public String toString() {
+    return "SystemUnderTest";
+  }
+  
+  
+}
