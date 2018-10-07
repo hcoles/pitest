6662d992052838eda8f9d3f7388727c2e65e9ba2
Emil Bremer Orloff <ebo@secata.com>
2018-10-07 16:14:13 +0200

    Added full test range for methods invoked through invokeDynamic

--- pitest/src/test/java/org/pitest/mutationtest/engine/gregor/mutators/BigIntegerMutatorTest.java
+++ pitest/src/test/java/org/pitest/mutationtest/engine/gregor/mutators/BigIntegerMutatorTest.java
@@ -18,6 +18,9 @@ import java.math.BigInteger;
 import java.util.Collection;
 import java.util.concurrent.Callable;
 import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Supplier;
 import org.junit.Before;
 import org.junit.Test;
 import org.pitest.mutationtest.engine.Mutant;
@@ -189,10 +192,159 @@ public class BigIntegerMutatorTest extends MutatorTestBase {
   }
 
   @Test
+  public void addLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(AddLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new AddLambda(0, 2), mutant, "-2");
+  }
+
+  @Test
+  public void subtractLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(SubtractLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new SubtractLambda(0, 3), mutant, "3");
+  }
+
+  @Test
+  public void mutliplyLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(MultiplyLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new MultiplyLambda(2, 2), mutant, "1");
+  }
+
+  @Test
+  public void divideLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(DivideLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new DivideLambda(2, 2), mutant, "4");
+  }
+
+  @Test
   public void moduloLambda() throws Exception {
     final Collection<MutationDetails> actual = findMutationsFor(ModuloLambda.class);
     final Mutant mutant = getFirstMutant(actual);
-    assertMutantCallableReturns(new ModuloLambda(25, 6), mutant, "150");
+    assertMutantCallableReturns(new ModuloLambda(25, 5), mutant, "125");
+  }
+
+  @Test
+  public void absLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(AbsLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new AbsLambda(-25, 6), mutant, "25");
+    assertMutantCallableReturns(new AbsLambda(25, 6), mutant, "-25");
+  }
+
+  @Test
+  public void notLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(NotLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new NotLambda(0b1111), mutant, String.valueOf(-0b1111));
+    assertMutantCallableReturns(new NotLambda(0b1111), mutant, String.valueOf(-0b1111));
+  }
+
+  @Test
+  public void negateLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(NegateLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new NegateLambda(0xFFFFFF00), mutant, String.valueOf(0x000000FF));
+    assertMutantCallableReturns(new NegateLambda(0), mutant, String.valueOf(0xFFFFFFFF));
+    assertMutantCallableReturns(new NegateLambda(0xFF00FF00), mutant, String.valueOf(0x00FF00FF));
+  }
+
+  @Test
+  public void minLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(MinLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new MinLambda(-25, 6), mutant, "6");
+    assertMutantCallableReturns(new MinLambda(25, 6), mutant, "25");
+  }
+
+  @Test
+  public void maxLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(MaxLambda.class);
+    final Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new MaxLambda(-25, 6), mutant, "-25");
+    assertMutantCallableReturns(new MaxLambda(25, 6), mutant, "6");
+  }
+
+  @Test
+  public void setBitLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(SetBitLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new SetBitLambda(0b11101, 2), mutant, String.valueOf(0b11001));
+    assertMutantCallableReturns(new SetBitLambda(0b11001, 2), mutant, String.valueOf(0b11001));
+  }
+
+  @Test
+  public void clearBitLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(ClearBitLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new ClearBitLambda(0b11101, 2), mutant, String.valueOf(0b11101));
+    assertMutantCallableReturns(new ClearBitLambda(0b01101, 0), mutant, String.valueOf(0b01101));
+  }
+
+  @Test
+  public void flipBitLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(FlipBitLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new FlipBitLambda(0b11101, 2), mutant, String.valueOf(0b11101));
+    assertMutantCallableReturns(new FlipBitLambda(0b01101, 0), mutant, String.valueOf(0b01101));
+  }
+
+  @Test
+  public void shiftLeftLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(ShiftLeftLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new ShiftLeftLambda(1, 1), mutant, String.valueOf(1 >> 1));
+    assertMutantCallableReturns(new ShiftLeftLambda(1, 2), mutant, String.valueOf(1 >> 2));
+    assertMutantCallableReturns(new ShiftLeftLambda(1 << 8, 8), mutant, String.valueOf(1));
+    assertMutantCallableReturns(new ShiftLeftLambda(1 << 8, 4), mutant, String.valueOf(1 << 8 >> 4));
+  }
+
+  @Test
+  public void shiftRightLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(ShiftRightLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new ShiftRightLambda(1, 1), mutant, String.valueOf(1 << 1));
+    assertMutantCallableReturns(new ShiftRightLambda(1, 2), mutant, String.valueOf(1 << 2));
+    assertMutantCallableReturns(new ShiftRightLambda(1 << 8, 8), mutant, String.valueOf(1L << 16L));
+    assertMutantCallableReturns(new ShiftRightLambda(1 << 8, 4), mutant, String.valueOf(1 << 8 << 4));
+  }
+
+  @Test
+  public void andLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(AndLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new AndLambda(0b01, 0b10), mutant, String.valueOf(0b11));
+    assertMutantCallableReturns(new AndLambda(0b01, 0b01), mutant, String.valueOf(0b01));
+    assertMutantCallableReturns(new AndLambda(0b01, 0b00), mutant, String.valueOf(0b01));
+  }
+
+  @Test
+  public void orLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(OrLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new OrLambda(0b01, 0b10), mutant, String.valueOf(0b00));
+    assertMutantCallableReturns(new OrLambda(0b01, 0b01), mutant, String.valueOf(0b01));
+    assertMutantCallableReturns(new OrLambda(0b11, 0b10), mutant, String.valueOf(0b10));
+  }
+
+  @Test
+  public void xorLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(XorLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new XorLambda(0b01, 0b10), mutant, String.valueOf(0b00));
+    assertMutantCallableReturns(new XorLambda(0b01, 0b01), mutant, String.valueOf(0b01));
+    assertMutantCallableReturns(new XorLambda(0b11, 0b10), mutant, String.valueOf(0b10));
+  }
+
+  @Test
+  public void andNotLambda() throws Exception {
+    final Collection<MutationDetails> actual = findMutationsFor(AndNotLambda.class);
+    Mutant mutant = getFirstMutant(actual);
+    assertMutantCallableReturns(new AndNotLambda(0b01, 0b10), mutant, String.valueOf(0b00));
+    assertMutantCallableReturns(new AndNotLambda(0b01, 0b01), mutant, String.valueOf(0b01));
+    assertMutantCallableReturns(new AndNotLambda(0b11, 0b10), mutant, String.valueOf(0b10));
   }
 
   private static abstract class AbstractMath implements Callable<String> {
@@ -439,6 +591,230 @@ public class BigIntegerMutatorTest extends MutatorTestBase {
     BigInteger apply(BigInteger left, BigInteger right) {
       return left.abs();
     }
+  }
+
+  private static class AddLambda extends AbstractMath {
+
+    AddLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::add;
+      return function.apply(left, right);    }
+  }
+
+  private static class SubtractLambda extends AbstractMath {
+
+    SubtractLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::subtract;
+      return function.apply(left, right);    }
+  }
+
+  private static class DivideLambda extends AbstractMath {
+
+    DivideLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::divide;
+      return function.apply(left, right);
+    }
+  }
+
+  private static class MultiplyLambda extends AbstractMath {
+
+    MultiplyLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::multiply;
+      return function.apply(left, right);
+    }
+  }
+
+  private static class SetBitLambda extends AbstractMath {
+
+    SetBitLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      IntFunction<BigInteger> function = left::setBit;
+      return function.apply(right.intValue());
+    }
+  }
+
+  private static class ClearBitLambda extends AbstractMath {
+
+    ClearBitLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      IntFunction<BigInteger> function = left::clearBit;
+      return function.apply(right.intValue());
+    }
+  }
+
+  private static class FlipBitLambda extends AbstractMath {
+
+    FlipBitLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      IntFunction<BigInteger> function = left::flipBit;
+      return function.apply(right.intValue());
+    }
+  }
+
+  private static class ShiftLeftLambda extends AbstractMath {
+
+    ShiftLeftLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      IntFunction<BigInteger> function = left::shiftLeft;
+      return function.apply(right.intValue());    }
+  }
+
+  private static class ShiftRightLambda extends AbstractMath {
+
+    ShiftRightLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      IntFunction<BigInteger> function = left::shiftRight;
+      return function.apply(right.intValue());    }
+  }
+
+  private static class AndLambda extends AbstractMath {
+
+    AndLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::and;
+      return function.apply(left, right);
+    }
+  }
+
+  private static class OrLambda extends AbstractMath {
+
+    OrLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::or;
+      return function.apply(left, right);    }
+  }
+
+  private static class XorLambda extends AbstractMath {
+
+    XorLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::xor;
+      return function.apply(left, right);    }
+  }
+
+  private static class AndNotLambda extends AbstractMath {
+
+    AndNotLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::andNot;
+      return function.apply(left, right);    }
+  }
+
+  private static class MaxLambda extends AbstractMath {
+
+    MaxLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::max;
+      return function.apply(left, right);    }
+  }
+
+  private static class MinLambda extends AbstractMath {
+
+    MinLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      BiFunction<BigInteger, BigInteger, BigInteger> function = BigInteger::min;
+      return function.apply(left, right);    }
+  }
+
+  private static class NotLambda extends AbstractMath {
+
+    NotLambda(long v1) {
+      super(v1, 0L);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      Supplier<BigInteger> function = left::not;
+      return function.get();
+    }
+  }
+
+  private static class NegateLambda extends AbstractMath {
+
+    NegateLambda(long v1) {
+      super(v1, 0L);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      Function<BigInteger, BigInteger> function = BigInteger::negate;
+      return function.apply(left);
+    }
+  }
+
+  private static class AbsLambda extends AbstractMath {
+
+    AbsLambda(long v1, long v2) {
+      super(v1, v2);
+    }
+
+    @Override
+    BigInteger apply(BigInteger left, BigInteger right) {
+      Function<BigInteger, BigInteger> function = BigInteger::abs;
+      return function.apply(left);    }
   }
 
   private static class ModuloLambda extends AbstractMath {
