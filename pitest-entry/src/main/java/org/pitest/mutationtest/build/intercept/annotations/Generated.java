e1d409450a44209bd8c56d5e46745e03d97046fe
Henry Coles <henry@pitest.org>
2025-05-09 11:47:37 +0100

    switch to central publishing

---
+++ pitest-entry/src/main/java/org/pitest/mutationtest/build/intercept/annotations/Generated.java
@@ -0,0 +1,8 @@
+package org.pitest.mutationtest.build.intercept.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.CLASS)
+public @interface Generated {
+}
