7e79934001977464db8e91679c2c557c29d1f63e
Henry Coles <henry@pitest.org>
2026-05-26 11:14:04 +0100

    make report options available to history

--- pitest-entry/src/main/java/org/pitest/mutationtest/HistoryParams.java
+++ pitest-entry/src/main/java/org/pitest/mutationtest/HistoryParams.java
@@ -1,14 +1,17 @@
 package org.pitest.mutationtest;
 
 import org.pitest.classpath.CodeSource;
+import org.pitest.mutationtest.config.ReportOptions;
 import org.pitest.plugin.FeatureSelector;
 public class HistoryParams {
     private final FeatureSelector conf;
     private final CodeSource code;
+    private final ReportOptions data;
 
-    public HistoryParams(FeatureSelector conf, CodeSource code) {
+    public HistoryParams(FeatureSelector conf, CodeSource code, ReportOptions data) {
         this.conf = conf;
         this.code = code;
+        this.data = data;
     }
 
     public FeatureSelector featureSettings() {
@@ -17,5 +20,9 @@ public class HistoryParams {
 
     public CodeSource code() {
         return code;
+    }
+
+    public ReportOptions data() {
+        return data;
     }
 }
