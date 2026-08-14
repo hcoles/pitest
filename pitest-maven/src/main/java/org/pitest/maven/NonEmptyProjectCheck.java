24f58058b48853aafa4d4599bebfeefe463fa15b
Henry Coles <henry@pitest.org>
2026-08-14 21:27:04 +0100

    check directories populated when checking for empty projects

--- pitest-maven/src/main/java/org/pitest/maven/NonEmptyProjectCheck.java
+++ pitest-maven/src/main/java/org/pitest/maven/NonEmptyProjectCheck.java
@@ -1,9 +1,10 @@
 package org.pitest.maven;
 
-import java.io.File;
-
 import org.apache.maven.project.MavenProject;
-import org.pitest.functional.FCollection;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Predicate;
 
 public class NonEmptyProjectCheck implements Predicate<MavenProject> {
@@ -11,12 +12,23 @@ public class NonEmptyProjectCheck implements Predicate<MavenProject> {
   @SuppressWarnings("unchecked")
   @Override
   public boolean test(MavenProject project) {
-    return FCollection.contains(project.getTestCompileSourceRoots(), exists()) 
-        && FCollection.contains(project.getCompileSourceRoots(), exists());
+    return project.getTestCompileSourceRoots().stream().anyMatch(this::exists)
+        && project.getCompileSourceRoots().stream().anyMatch(this::exists);
   }
   
-  private Predicate<String> exists() {
-    return root -> new File(root).exists();
+  private boolean exists(String root) {
+    var p = Path.of(root);
+      try {
+          return Files.isDirectory(p) && isPopulated(p);
+      } catch (IOException e) {
+          throw new RuntimeException(e);
+      }
+  }
+
+  private static boolean isPopulated(Path p) throws IOException {
+    try (var list = Files.list(p)) {
+      return list.findAny().isPresent();
+    }
   }
 
 }
--- pitest-maven/src/test/java/org/pitest/maven/NonEmptyProjectCheckIT.java
+++ pitest-maven/src/test/java/org/pitest/maven/NonEmptyProjectCheckIT.java
@@ -4,39 +4,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 
 import org.apache.maven.project.MavenProject;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.TemporaryFolder;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mockito;
+
 
 @Category(SystemTest.class)
-@RunWith(MockitoJUnitRunner.class)
 public class NonEmptyProjectCheckIT {
 
   @Rule
   public TemporaryFolder realDir = new TemporaryFolder();
+
+  @Rule
+  public TemporaryFolder emptyDir = new TemporaryFolder();
   
   NonEmptyProjectCheck testee = new NonEmptyProjectCheck();
-  
-  @Mock
-  private MavenProject project;
-  
+
+  private MavenProject project = Mockito.mock(MavenProject.class);
+
+  @Before
+  public void populateDirectory() throws IOException {
+    realDir.newFile("temp");
+  }
+
   @Test
   public void shouldTreatProjectWithCodeAndTestsAsNonEmpty() {
-    when(project.getTestCompileSourceRoots()).thenReturn(Collections.singletonList(dirThatExists()));
-    when(project.getCompileSourceRoots()).thenReturn(Collections.singletonList(dirThatExists()));
+    when(project.getTestCompileSourceRoots()).thenReturn(Collections.singletonList(dirWithContents()));
+    when(project.getCompileSourceRoots()).thenReturn(Collections.singletonList(dirWithContents()));
     assertThat(testee.test(project)).isTrue();
   }
-  
+
+
   @Test
   public void shouldTreatProjectWithNoTestsAsEmpty() {
-    when(project.getTestCompileSourceRoots()).thenReturn(Collections.singletonList(dirThatExists()));
+    when(project.getTestCompileSourceRoots()).thenReturn(Collections.singletonList(dirWithContents()));
     when(project.getCompileSourceRoots()).thenReturn(Collections.singletonList(nonExistentDir()));
     assertThat(testee.test(project)).isFalse();
   }
@@ -44,12 +52,24 @@ public class NonEmptyProjectCheckIT {
   @Test
   public void shouldTreatProjectWithNoCodeAsEmpty() {
     when(project.getTestCompileSourceRoots()).thenReturn(Collections.singletonList(nonExistentDir()));
-    when(project.getCompileSourceRoots()).thenReturn(Collections.singletonList(dirThatExists()));
+    when(project.getCompileSourceRoots()).thenReturn(Collections.singletonList(dirWithContents()));
     assertThat(testee.test(project)).isFalse();
   }
-  
-  private String dirThatExists() {
+
+
+  @Test
+  public void emptyDirectoriesAreTreatedAsEmpty() {
+    when(project.getTestCompileSourceRoots()).thenReturn(Collections.singletonList(emptyDir()));
+    when(project.getCompileSourceRoots()).thenReturn(Collections.singletonList(dirWithContents()));
+    assertThat(testee.test(project)).isFalse();
+  }
+
+  private String dirWithContents() {
     return realDir.getRoot().getAbsolutePath();
+  }
+
+  private String emptyDir() {
+    return emptyDir.getRoot().getAbsolutePath();
   }
   
   private String nonExistentDir() {
