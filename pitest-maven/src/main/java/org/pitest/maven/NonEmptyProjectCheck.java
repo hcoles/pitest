0a445178af8a4e2bd346322b183d54cdb008bb21
Henry Coles <henry@pitest.org>
2026-08-25 16:26:45 +0100

    check existence of groovy dirs
    Many groovy projects do not have source dirs properly configured, triggering the empty
    project check so we must look for them explicitly at their standard location.

--- pitest-maven/src/main/java/org/pitest/maven/NonEmptyProjectCheck.java
+++ pitest-maven/src/main/java/org/pitest/maven/NonEmptyProjectCheck.java
@@ -2,9 +2,12 @@ package org.pitest.maven;
 
 import org.apache.maven.project.MavenProject;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
 
 public class NonEmptyProjectCheck implements Predicate<MavenProject> {
@@ -12,10 +15,27 @@ public class NonEmptyProjectCheck implements Predicate<MavenProject> {
   @SuppressWarnings("unchecked")
   @Override
   public boolean test(MavenProject project) {
-    return project.getTestCompileSourceRoots().stream().anyMatch(this::exists)
-        && project.getCompileSourceRoots().stream().anyMatch(this::exists);
+    var baseDir = project.getBasedir() != null ? project.getBasedir() : new File("");
+    return augmentTests(baseDir, project.getTestCompileSourceRoots()).stream().anyMatch(this::exists)
+        && augmentMain(baseDir, project.getCompileSourceRoots()).stream().anyMatch(this::exists);
   }
-  
+
+  // maven projects for other jvm languages are often not properly configured
+  // and don't declare their source directories
+  private List<String> augmentTests(File basedir, List<String> existing) {
+    var groovy = basedir.toPath().resolve("src/test/groovy");
+    var updated = new ArrayList<>(existing);
+    updated.add(groovy.toAbsolutePath().toString());
+    return updated;
+  }
+
+  private List<String> augmentMain(File basedir, List<String> existing) {
+    var groovy = basedir.toPath().resolve("src/main/groovy");
+    var updated = new ArrayList<>(existing);
+    updated.add(groovy.toAbsolutePath().toString());
+    return updated;
+  }
+
   private boolean exists(String root) {
     var p = Path.of(root);
       try {
